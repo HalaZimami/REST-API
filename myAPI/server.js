f9cc8d6c8126cee1614e50deaefead4186005ec6
hala <halazimami@gmail.com>
2023-08-07 14:44:32 +0300

    updates

---
+++ myAPI/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+const cars = require('./cars.json');
+app.get('/api/cars', (req, res) => {
+    res.json(cars);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on Port: ${port}`)
+});
