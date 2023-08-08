e8dc26f4dd772ec5d2d21446e66c82631477b913
hala <halazimami@gmail.com>
2023-08-08 09:41:01 +0300

    updated post function to add or modify localAPI

--- myAPI/server.js
+++ myAPI/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
@@ -8,8 +9,16 @@ app.get('/api/cars', (req, res) => {
     res.json(cars);
 });
 
-
-
 app.listen(port, () => {
     console.log(`Server is listening on Port: ${port}`)
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.post('/api/cars', (req, res) => {
+    const newCar = req.body;
+    newCar.id = cars.length + 1;
+    cars.push(newCar);
+    res.json({ message: 'Car added!', car: newCar });
+
+})
